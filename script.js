4e242555d5738a9ed81d25233d132d6a00488e08
Dmitry73109 <dmitry73109@gmail.com>
2025-04-15 09:30:20 +0300

    Release version

--- script.js
+++ script.js
@@ -37,9 +37,12 @@ function renderContacts() {
                     <h5>${contact.name}</h5>
                     <p>${contact.phone}</p>
                 </div>
-                <div class="contact-actions">
-                    <i class="ri-edit-line edit-icon" data-index="${index}"></i>
-                    <i class="ri-subtract-line delete-icon" data-index="${index}"></i>
+                <div class="contact-actions dropdown">
+                    <i class="ri-more-2-fill" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item edit-contact" data-index="${index}" href="#">Изменить</a></li>
+                        <li><a class="dropdown-item delete-contact" data-index="${index}" href="#">Удалить</a></li>
+                    </ul>
                 </div>
             </div>
         `);
@@ -47,7 +50,7 @@ function renderContacts() {
         $list.append($card);
     });
 
-    $('.edit-icon').on('click', function (e) {
+    $('.edit-contact').on('click', function (e) {
         e.stopPropagation();
         const index = $(this).data('index');
         const contact = contacts[index];
@@ -55,30 +58,17 @@ function renderContacts() {
         $('#nameInput').val(contact.name);
         $('#emailInput').val(contact.email);
         $('#phoneInput').val(contact.phone);
-        $('#contactIndex').val(index);//all info need to restore info for editing
+        $('#contactIndex').val(index); //all info need to restore info for editing
 
         contactModal.show();
     });
 
-    $('.delete-icon').on('click', function (e) {
-        e.stopPropagation();//will delete that data where was pressed
+    $('.delete-contact').on('click', function (e) {
+        e.stopPropagation(); //will delete that data where was pressed
         const index = $(this).data('index');
         contacts.splice(index, 1);
         localStorage.setItem('contacts', JSON.stringify(contacts));
         renderContacts();
-    });
-
-    $('.contact-card').on('click', function () {
-        const index = $(this).data('index');
-        const contact = contacts[index];
-        Swal.fire({
-            title: contact.name,
-            html: `
-                <p><strong>Email:</strong> ${contact.email}</p>
-                <p><strong>Phone:</strong> ${contact.phone}</p>
-            `,
-            icon: 'info'
-        });
     });
 }
 
@@ -91,7 +81,7 @@ function getColor(name) {
     return colors[hash % colors.length];
 }
 
-$('#addContactBtn').on('click', function () {//this one just clear table
+$('#addContactBtn').on('click', function () { //this one just clear table
     $('#contactForm')[0].reset();
     $('#contactIndex').val('');
     contactModal.show();
@@ -99,14 +89,14 @@ $('#addContactBtn').on('click', function () {//this one just clear table
 
 $('#contactForm').on('submit', function (e) {
     e.preventDefault();
-//GET VALUES
+    //GET VALUES
     const name = $('#nameInput').val();
     const email = $('#emailInput').val();
     const phone = $('#phoneInput').val();
     const index = $('#contactIndex').val();
-//NAME TEST##########################################################
-    const nameRegex = /[0-9!@#$%^&*()_+{}\[\]:;"'<>,.?\\\/|=`~]/;//allow only letters
-
+    
+    //NAME TEST##########################################################
+    const nameRegex = /[0-9!@#$%^&*()_+{}\[\]:;"'<>,.?\\\/|=~]/; //allow only letters
     if(nameRegex.test(name)){
         alert("Wrong name, only letters allowed");
         return;
@@ -114,9 +104,9 @@ $('#contactForm').on('submit', function (e) {
         alert("Wrong length, 15 letters only allowed");
         return;
     }
-//EMAIL TEST##########################################################
-    const emailRegex = /[!@#$%^&*()_+{}\[\]:;"'<>,.?\\\/|-]/;//test like: text@text.text
 
+    //EMAIL TEST##########################################################
+    const emailRegex = /[!@#$%^&*()_+{}\[\]:;"'<>,.?\\\/|-]/; //test like: text@text.text
     if(!emailRegex.test(email)){
         alert("Wrong email, only -> test@test.test");
         return;
@@ -124,9 +114,9 @@ $('#contactForm').on('submit', function (e) {
         alert("Wrong length, 9-24 only allowed");
         return;
     }
-//PHONE TEST##########################################################
-    const phoneRegex = /^\+?[0-9]{7,14}$/;//allow 1 plus at 1rst possition and numbers after
 
+    //PHONE TEST##########################################################
+    const phoneRegex = /^\+?[0-9]{7,14}$/; //allow 1 plus at 1rst possition and numbers after
     if(!phoneRegex.test(phone)){
         alert("Wrong phone number, only -> +1234567890");
         return;
@@ -134,16 +124,17 @@ $('#contactForm').on('submit', function (e) {
         alert("Wrong length, only 9-13 allowed");
         return;
     }
-//ADD OR EDIT##########################################################
+
+    //ADD OR EDIT##########################################################
     const newContact = { name, email, phone };
 
-    if (index === '') {//IF EMPTY -> CREATE NEW
-        contacts.push(newContact);//NO INDEX, FORM WILL BE EMPTY
-    } else {//IF NOT -> EDIT OLD
-        contacts[index] = newContact;//INDEX HELPS TO RESTORE INFO TO EDIT IT
+    if (index === '') { //IF EMPTY -> CREATE NEW
+        contacts.push(newContact); //NO INDEX, FORM WILL BE EMPTY
+    } else { //IF NOT -> EDIT OLD
+        contacts[index] = newContact; //INDEX HELPS TO RESTORE INFO TO EDIT IT
     }
 
-    localStorage.setItem('contacts', JSON.stringify(contacts));//SAVE ALL INFO LIKE STRING
-    renderContacts();//CLEAR TABLE AND CREATE NEW ONE
-    contactModal.hide();//HIDE MENU
+    localStorage.setItem('contacts', JSON.stringify(contacts)); //SAVE ALL INFO LIKE STRING
+    renderContacts(); //CLEAR TABLE AND CREATE NEW ONE
+    contactModal.hide(); //HIDE MENU
 });
